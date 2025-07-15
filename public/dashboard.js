e51a8c7ca40ea9b823ffffd9a91581972a28080e
Luisvaldes1234 <luisgerardo.euromobile@gmail.com>
2025-07-15 04:00:35 -0600

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -218,7 +218,7 @@ async function cargarResumen() {
         renderResumenPorMaquina(resumenMaquina, contenedorMaquinas);
 
     });
-        renderMachineMap(maquinas, allSales || []);
+        renderMachineMap(maquinas, ventasDelMesCompleto || []);
 
 
 }
